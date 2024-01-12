a73562400adf0bde3d3e036e7d6ad3ff841029a7
rajamishra2209 <146928228+rajamishra2209@users.noreply.github.com>
2024-01-13 03:45:49 +0530

    Update Details.jsx

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
@@ -8,9 +7,8 @@ import VideosSection from "./videoSection/VideoSection";
 import Similar from "./carousels/Similar";
 import Recommendation from "./carousels/Recommendation";
 
-
 const Details = () => {
-  const { mediaType } = useParams();
+  const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
   const { data: credits, loading: creditsLoading } = useFetch(
     `/${mediaType}/${id}/credits`
@@ -21,8 +19,8 @@ const Details = () => {
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
       <VideosSection data={data} loading={loading} />
-      <Similar mediaType={mediaType} id={id}/>
-      <Recommendation mediaType={mediaType} id={id}/>
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   );
 };
